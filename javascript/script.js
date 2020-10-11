9d7cd9864993411fd331c5e863106753a4df6595
MaidanaGustavo <mr.xibisnet@gmail.com>
2020-10-10 20:15:58 -0400

    renderizacao da img

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,7 @@
 let fun = function (sucess) {
+    
+    
+    let contagem =0;
 
     setInterval(aguardarStatus,1000);
      function  aguardarStatus(){
@@ -9,23 +12,31 @@ let fun = function (sucess) {
   
         function render(res){
           if(res.status == 'finished'){
+            document.getElementById('counter').innerHTML = "";  
             Vue.component("imagem", {
               props: ["img"],
               template: `<img src="https://browshot.com/screenshot/image/${res.id}" id="img" />`,
             });
             let img = new Vue({
-              el: "#img",
-            });
+                el: "#imga",
+      
+              });
+        }else{
+            contagem++;
+            document.getElementById('counter').innerHTML = contagem;
+            
           }
         }
-     }
-    
-     
-    
+    }    
   };
   function takeaShot(){
    let url = document.getElementById('url1').value 
-    let create = fetch(`https://api.browshot.com/api/v1/screenshot/create?url=${url}/&instance_id=12&size=screen&cache=0&key=DW0ZLoI5v4ksqdQM8GP8jnsq2iX`)
+   console.log(url)
+   
+   let create = fetch(`https://api.browshot.com/api/v1/screenshot/create?url=${url}/&instance_id=12&size=screen&cache=0&key=DW0ZLoI5v4ksqdQM8GP8jnsq2iX`)
     .then((data) => data.json())
     .then((sucess) => fun(sucess));
+
 }
+
+
